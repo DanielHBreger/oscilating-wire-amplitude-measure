f7cf939b6248c4d12a4e536f746c4463aeb33ca1
Daniel Haim Breger <danib577@gmail.com>
2025-01-02 11:56:10 +0200

    Convert to work with .arw files instead of .mov

--- calibration.py
+++ calibration.py
@@ -7,26 +7,32 @@ import threading
 import queue
 import math
 import time
+import rawpy
+import os
 
-filename = 'WhatsApp Video 2024-12-26 at 14.22.04.mp4'
+folder = 'try1/mode 7'
+# folder = os.fsencode(folder)
+files = [name for name in os.listdir(folder)]
+# files = [f for f in files if f=='DSC00279.ARW']
+print(files)
 sample_rate = 1
-video = cv2.VideoCapture(filename)
-total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-x,y,wi,h = 0, 100, 600, 100
-threshold = 220
+total_frames = len(files)
+x,y,wi,h = 2000,2300,3000,200
+threshold = 250
 
-for fno in range(int(total_frames/2),total_frames,sample_rate):
-    cap = cv2.VideoCapture(filename)
-    cap.set(cv2.CAP_PROP_POS_FRAMES, fno)
-    success, image = cap.read()
-    img = image.astype("uint8")
-    img = img[y:y+h,x:x+wi]
-    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    # thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,117,100)
-    ret, thresh = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
-    # blur = cv2.GaussianBlur(thresh,(5,5),0)
-    # ret, thresh = cv2.threshold(blur, 240, 255, cv2.THRESH_BINARY)
-    invert = 255 - thresh
-    cv2.imshow('image', invert)
-    cv2.waitKey()
+for file in files:
+    fname = folder+'/'+file
+    with open(fname, 'rb') as f:
+        with rawpy.imread(f) as raw:
+            image = raw.postprocess(use_camera_wb=True)
+            img = image.astype("uint8")
+            img = img[y:y+h,x:x+wi]
+            gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+            # blur = cv2.GaussianBlur(gray,(5,5),0)
+            # thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,117,100)
+            ret, thresh = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
+            blur = cv2.GaussianBlur(thresh,(5,5),0)
+            ret, thresh = cv2.threshold(blur, 254, 255, cv2.THRESH_BINARY)
+            invert = 255 - thresh
+            cv2.imshow('image', invert)
+            cv2.waitKey()
--- measure-amp.py
+++ measure-amp.py
@@ -8,9 +8,14 @@ import queue
 import math
 import time
 import argparse
+import os
+import rawpy
+import re
+
+import rawpy._rawpy
 
 parser = argparse.ArgumentParser(description='Measure amplitude of a video')
-parser.add_argument('filename', type=str, help='The filename of the video')
+parser.add_argument('arw_path', type=str, help='The path of the raw files')
 parser.add_argument('--sample_rate', type=int, default=1,
                     help='The sample rate to use (in frames)')
 parser.add_argument('--x', type=int, default=0,
@@ -38,50 +43,55 @@ class Worker(threading.Thread):
         super().__init__(group, target, name, args, kwargs, daemon=daemon)
         self.queue = queue.Queue(maxsize=20)
 
-    def measure(self, video_path, fnos, callback):
-        self.queue.put((video_path, fnos, callback))
+    def measure(self, files, callback):
+        self.queue.put((files, callback))
 
     def run(self):
-        video_path, fnos, callback = self.queue.get()
-        cap = cv2.VideoCapture(video_path)
-        for fno in fnos:
-            blackpoints = []
-            cap.set(cv2.CAP_PROP_POS_FRAMES, fno)
-            success, image = cap.read()
-            img = image.astype("uint8")
-            img = img[args.y:args.y+args.h, args.x:args.x+args.wi]
-            gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            blur = cv2.GaussianBlur(gray, (5, 5), 0)
-            # thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,117,100)
-            ret, thresh = cv2.threshold(
-                gray, args.threshold, 255, cv2.THRESH_BINARY)
-            if args.double_threshold:
-                blur = cv2.GaussianBlur(thresh, (5, 5), 0)
-                ret, thresh = cv2.threshold(blur, 240, 255, cv2.THRESH_BINARY)
-            invert = 255 - thresh
-            for ix, line in enumerate(invert):
-                for iy, pixel in enumerate(line):
-                    if pixel == 0:
-                        blackpoints.append((iy, ix))
-            while True:
-                maxy = max(blackpoints, key=itemgetter(1))
-                miny = min(blackpoints, key=itemgetter(1))
-                if maxy[0]-miny[0] > 200:
-                    blackpoints.remove(maxy)
-                    blackpoints.remove(miny)
-                else:
-                    break
-            callback(fno, maxy[1]-miny[1])
+        files, callback = self.queue.get()
+        for idx, filename in enumerate(files):
+            with open(filename, 'rb') as f:
+                try:
+                    with rawpy.imread(f) as raw:
+                        img = raw.postprocess(use_camera_wb=True)
+                        blackpoints = []
+                        img = img[args.y:args.y+args.h, args.x:args.x+args.wi]
+                        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+                        # blur = cv2.GaussianBlur(gray, (5, 5), 0)
+                        # thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,117,100)
+                        ret, thresh = cv2.threshold(
+                            gray, args.threshold, 255, cv2.THRESH_BINARY)
+                        if args.double_threshold:
+                            blur = cv2.GaussianBlur(thresh, (5, 5), 0)
+                            ret, thresh = cv2.threshold(blur, 250, 255, cv2.THRESH_BINARY)
+                        invert = 255 - thresh
+                        for ix, line in enumerate(invert):
+                            for iy, pixel in enumerate(line):
+                                if pixel == 0:
+                                    blackpoints.append((iy, ix))
+                        while True:
+                            maxy = max(blackpoints, key=itemgetter(1))
+                            miny = min(blackpoints, key=itemgetter(1))
+                            if maxy[0]-miny[0] > 200:
+                                blackpoints.remove(maxy)
+                                blackpoints.remove(miny)
+                            else:
+                                break
+                        filenumber = re.findall('([0-9]*)\.ARW$',filename)[0]
+                        filenumber = int(filenumber)
+                        callback(filenumber, maxy[1]-miny[1])
+                except rawpy._rawpy.LibRawFileUnsupportedError as e:
+                    continue
 
 
 def main():
-    video = cv2.VideoCapture(args.filename)
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    files = [name for name in os.listdir(args.arw_path)]
+    files = [file for file in files if file.endswith('.ARW')]
+    total_frames = len(files)
     fnos = list(range(0, total_frames, args.sample_rate))
     tasks = [[] for _ in range(0, args.num_threads)]
     frames_per_thread = math.ceil(len(fnos)/args.num_threads)
     for idx, fno in enumerate(fnos):
-        tasks[math.floor(idx/frames_per_thread)].append(fno)
+        tasks[math.floor(idx/frames_per_thread)].append(args.arw_path+'/'+files[fno])
 
     threads = []
     for _ in range(0, args.num_threads):
@@ -97,7 +107,7 @@ def main():
     frequencies = []
 
     for idx, w in enumerate(threads):
-        w.measure(args.filename, tasks[idx], on_done)
+        w.measure(tasks[idx], on_done)
 
     while True:
         result = results.get(timeout=10)
@@ -107,12 +117,14 @@ def main():
         if len(ids) >= total_frames/args.sample_rate:
             break
 
+    ids = [-1*id for id in ids]
+
     plt.plot(ids, widths, marker='.', linestyle='none')
     plt.xlabel('Frame')
     plt.ylabel('Width (pixels)')
     plt.grid(True)
-    plt.savefig(f'{args.filename}-AMPS-{time.strftime("%Y%m%d-%H%M%S")}.png')
-    # plt.show()
+    plt.savefig(f'{args.arw_path}-AMPS-{time.strftime("%Y%m%d-%H%M%S")}.png')
+    plt.show()
 
 
 if __name__ == '__main__':
